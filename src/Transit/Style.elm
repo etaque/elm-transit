d99797ee6f778c4b1a8d893369b0ea10b1c48425
Emilien Taque <eta@zengularity.com>
2015-12-30 14:43:07 +0100

    Renaming and doc for Transit.Style

--- src/Transit/Style.elm
+++ src/Transit/Style.elm
@@ -1,22 +1,81 @@
 module Transit.Style
-  ( fadeSlideLeft,slideLeft, slideLeftExit, slideLeftEnter
-  , fade, fadeExit, fadeEnter
+  ( fadeSlideLeft
+  , slideLeft, slideOutLeft, slideInLeft
+  , fade, fadeOut, fadeIn
+  , transition, Style
   ) where
 
 {-| Default animations for transitions, to be used on elm-html `style` attribute.
 
+    div
+      [ style (TransitStyle.fadeSlideLeft 100 model) ]
+      [ text "content" ]
+
+# Compositions
+@docs fadeSlideLeft
+
 # Slide left
-@docs fadeSlideLeft, slideLeft, slideLeftExit, slideLeftEnter
+@docs slideLeft, slideOutLeft, slideInLeft
 
 # Fade
-@docs fade, fadeExit, fadeEnter
+@docs fade, fadeOut, fadeIn
+
+# Tooling to create animations
+@docs transition, Style
 -}
 
 import Transit exposing (..)
 
+
+{-| Alias for elm-html style value -}
 type alias Style = List (String, String)
 
 
+{-| Combine fade and slideLeft with the specified offset -}
+fadeSlideLeft : Float -> WithTransition m -> Style
+fadeSlideLeft offset m =
+  (slideLeft offset m) ++ (fade m)
+
+
+{-| Slide left transition, with the specified offset -}
+slideLeft : Float -> WithTransition m -> Style
+slideLeft offset m =
+  transition (slideOutLeft offset) (slideInLeft offset) m
+
+
+{-| Slide out to left (exit) -}
+slideOutLeft : Float -> Float -> Style
+slideOutLeft offset v =
+  [ ("transform", "translateX(" ++ toString (-v * offset) ++ "px)")
+  ]
+
+
+{-| Slide in to left (enter) -}
+slideInLeft : Float -> Float -> Style
+slideInLeft offset v =
+  [ ("transform", "translateX(" ++ toString (offset - v * offset) ++ "px)")
+  ]
+
+
+{-| Fade transition (out -> in) -}
+fade : WithTransition m -> Style
+fade m =
+  transition fadeOut fadeIn m
+
+
+{-| Fade out (exit) -}
+fadeOut : Float -> Style
+fadeOut v =
+  [ ("opacity", toString (1 - v)) ]
+
+
+{-| Fade in (enter) -}
+fadeIn : Float -> Style
+fadeIn v =
+  [ ("opacity", toString v) ]
+
+
+{-| Create an exit -> enter transition -}
 transition : (Float -> Style) -> (Float -> Style) -> WithTransition m -> Style
 transition exit enter model =
   case (getStatus model.transition, getValue model.transition) of
@@ -26,42 +85,3 @@ transition exit enter model =
       enter v
     _ ->
       []
-
-{-| TODO -}
-fadeSlideLeft : Float -> WithTransition m -> Style
-fadeSlideLeft offset m =
-  (slideLeft offset m) ++ (fade m)
-
-{-| TODO -}
-slideLeft : Float -> WithTransition m -> Style
-slideLeft offset m =
-  transition (slideLeftExit offset) (slideLeftEnter offset) m
-
-{-| TODO -}
-slideLeftExit : Float -> Float -> Style
-slideLeftExit offset v =
-  [ ("transform", "translateX(" ++ toString (-v * offset) ++ "px)")
-  ]
-
-{-| TODO -}
-slideLeftEnter : Float -> Float -> Style
-slideLeftEnter offset v =
-  [ ("transform", "translateX(" ++ toString (offset - v * offset) ++ "px)")
-  ]
-
-
-{-| TODO -}
-fade : WithTransition m -> Style
-fade m =
-  transition fadeExit fadeEnter m
-
-{-| Fade out exit -}
-fadeExit : Float -> Style
-fadeExit v =
-  [ ("opacity", toString (1 - v)) ]
-
-
-{-| Fade in enter -}
-fadeEnter : Float -> Style
-fadeEnter v =
-  [ ("opacity", toString v) ]
